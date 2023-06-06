555b4f4c63f3dddd5dcac75fe5dccda5894568a9
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-06-06 18:31:59 +0200

    Tried out some object literals

--- 1_basics_literals.js
+++ 1_basics_literals.js
@@ -9,3 +9,27 @@
 // console.log(window);
 // alert(1);
 // console.log(window.navigator.language);
+
+// Object Literal
+const book1 = {
+  title: 'Book One',
+  author: 'John Doe',
+  year: '2013',
+  getSummary() {
+    return `${this.title} was written by: ${this.author} in ${this.year}`;
+  },
+};
+
+const book2 = {
+  title: 'Book Two',
+  author: 'Jane Doe',
+  year: '2016',
+  getSummary() {
+    return `${this.title} was written by: ${this.author} in ${this.year}`;
+  },
+};
+
+// console.log(book1.getSummary());
+// console.log(book2.getSummary());
+// console.log(Object.keys(book2));
+// console.log(Object.values(book2));
