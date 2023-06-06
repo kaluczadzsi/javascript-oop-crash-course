a8f269f3b608dc7f8e1d2a031a71a4617e718b19
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-06-06 18:26:41 +0200

    Initial commit

---
+++ 1_basics_literals.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// const s1 = 'Hello';
+// console.log(typeof s1);
+
+// const s2 = new String('Hello');
+// console.log(typeof s2);
+
+// console.log(window);
+// alert(1);
+// console.log(window.navigator.language);
